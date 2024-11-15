93fb7be2765299795dfe739d6b8bfbda8ff49846
no-ctrl <57538189+no-ctrl@users.noreply.github.com>
2024-11-15 17:12:11 +0100

    //

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    reactStrictMode: true,
+    output: 'export', // Ова е за статичко извозење на проектот
+    basePath: '/password-evaluator', // Поставете го патот на GitHub Pages
+    trailingSlash: true, // Обезбедува да URL-то заврши со коса црта
+}
